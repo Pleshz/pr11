ef922e1cdf463c4b8a47a0bbdf35114ee5442949
Pleshz <pleskovd96@gmail.com>
2026-01-14 10:38:20 +0500

    Шифровка и расшифровка данных комментариев. Плешков

--- ajax/message.php
+++ ajax/message.php
@@ -2,8 +2,37 @@
     session_start();
 	include("../settings/connect_datebase.php");
 
+    function decryptAES($encryptedData, $key) {
+		$data = base64_decode($encryptedData);
+
+		if ($data === false || strlen($data) < 17) {
+			error_log("Invalid data or too short");
+			return false;
+		}
+
+		$iv = substr($data, 0, 16);
+		$encrypted = substr($data, 16);
+
+		$keyHash = md5($key);
+		$keyBytes = hex2bin($keyHash);
+
+		$decrypted = openssl_decrypt(
+			$encrypted,
+			'aes-128-cbc',
+			$keyBytes,
+			OPENSSL_RAW_DATA,
+			$iv
+		);
+
+		return $decrypted;
+	}
+
+    $message_encrypted = $_POST['Message'] ?? '';
+
+	$secretKey = "qazxswedcvfrtgbn";
+
     $IdUser = $_SESSION['user'];
-    $Message = $_POST["Message"];
+    $Message = decryptAES($message_encrypted, $secretKey);
     $IdPost = $_POST["IdPost"];
 
     $mysqli->query("INSERT INTO `comments`(`IdUser`, `IdPost`, `Messages`) VALUES ({$IdUser}, {$IdPost}, '{$Message}');");
